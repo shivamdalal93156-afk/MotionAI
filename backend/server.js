ecf911bfe9edc5eb378e9c93886cbad8ad02e45b
shivam-git <shivamdalal93156@gmail.com>
2026-05-08 17:24:08 +0530

    add ngrok headers

--- backend/server.js
+++ backend/server.js
@@ -22,8 +22,10 @@ app.use((req, res, next) => {
   next();
 });
 // ── Middleware ─────────────────────────────────────────────────
+
+
 app.use(cors({
-  origin: '*',
+  origin:'*',
 }));
 app.use((req, res, next) => {
   res.setHeader('ngrok-skip-browser-warning', 'true');
