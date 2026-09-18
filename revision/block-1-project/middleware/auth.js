49ab448d065eac49f24339b59eb3bec03de179fb
Shivam Kumar <shivamdalal93156@gmail.com>
2026-09-19 00:59:08 +0530

    Delete revision directory

--- revision/block-1-project/middleware/auth.js
+++
@@ -1,8 +0,0 @@
-const jwt = require("jsonwebtoken");
-
-async function tokencheck(req,res,next) {
-    const authtoken = req.token ;
-    if(!authtoken){
-        return res.status(401).send("inalid token");
-    }
-}
